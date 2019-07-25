7fbecb83c8e4d3ec6253aad1426c9b7e8954a967
peter max <petermax2004@gmail.com>
2019-07-25 11:17:19 +0300

    Add the tags class

--- IG_app/models.py
+++ IG_app/models.py
@@ -2,6 +2,11 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class tags(models.Model):
+    name=models.CharField(max_length=20)
+    
+   
+
 class Image(models.Model):
     image=models.ImageField(upload_to='/picture',)
     name=models.CharField(max_length=40)
