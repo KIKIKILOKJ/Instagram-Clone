b751fbbd40d247a9d2ac29d50c5dd48ac2b2ec62
peter max <petermax2004@gmail.com>
2019-07-25 11:57:51 +0300

    Add the class profile and fields:bio,profilepic,user,following,followers

--- IG_app/models.py
+++ IG_app/models.py
@@ -41,3 +41,10 @@ class Image(models.Model):
     
     def save_image(self):
         self.save()
+        
+class Profile(models.Model):
+    bio=models.TextField(max_length=100,null=True,blank=True,default="bio")
+    profilepic=models.ImageField(upload_to='picture/',null=True,blank=True)
+    user=models.OneToOneField(User,on_delete=models.CASCADE,blank=True,related_name="profile")
+    following=models.ManyToManyField(User,related_name="following",blank=True)
+    followers=models.ManyToManyField(User,related_name="followers",blank=True)
