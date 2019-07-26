0c0d8c1cbeb821d42d9d91a1dd3773276a4b5cba
peter max <petermax2004@gmail.com>
2019-07-26 18:28:50 +0300

    Modify the navbar so as to view the login log out and register elements

--- IG_app/urls.py
+++ IG_app/urls.py
@@ -5,4 +5,10 @@ urlpatterns=[
     url('^$',views.index,name = 'index.html'),
     url(r'^search/', views.search_users, name='search'),
     url(r'^new/image$', views.new_image, name='new_image'),
+    url(r'^image/(\d+)',views.image,name ='image'),
+    url(r'^profile/(?P<username>[0-9]+)$', views.person_profile_page, name='person_profile_page'),
+    url(r'^edit/profile$', views.edit_profile, name='edit_profile'),
+    url(r'^my_profile/$', views.my_profile, name='my_profile'),
+    url(r'^new/image$', views.new_image, name='new_image'),
+
 ]
