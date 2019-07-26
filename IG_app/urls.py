c99e4aeac1d0f66c055331fbac189ead6a1ebc3b
peter max <petermax2004@gmail.com>
2019-07-26 16:23:31 +0300

    Add the new image url pattern

--- IG_app/urls.py
+++ IG_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns=[
     url('^$',views.index,name = 'index.html'),
-     url(r'^search/', views.search_users, name='search'),
+    url(r'^search/', views.search_users, name='search'),
+    url(r'^new/image$', views.new_image, name='new_image'),
 ]
